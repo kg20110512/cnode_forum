80aac8ed5d3311e88def2548c037f943648f6b70
zhangsan <zhangsan@bikeman.com>
2018-04-19 16:21:47 +0800

    mock data add prop

--- mockdata/detail.js
+++ mockdata/detail.js
@@ -7,9 +7,17 @@ module.exports = {
       loginname: 'zhangsan',
       avatar_url: ''
     },
+    author_id: 'xxxxxxx',
+    title: '标题在此',
     content: '事件 什么是事件 事件是视图层到逻辑层的通讯方式。 事件可以将用户的行为反馈到逻辑层进行处理。 事件可以绑定在组件上，当达到触发事件，就会执行逻辑层中对应的事件处理函数。 事件对象可以携带额外信息，如 id, dataset, touches。 事件的使用方式 在组件中绑定一个事件处理函数。 如bindtap，当用户点击该组件的时候会在该页面对应的Page中找到相应的事件处理函数',
     is_collect:false,
-    tab: 'ask'
+    tab: 'ask',
+    top: true,
+    goot: true,
+    visit_count: 99,
+    reply_count: 10,
+    replies:[],
+    id: 'abcdefghijklmn'
   }
 
 }
