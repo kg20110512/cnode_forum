60aa8d8d4953984187c243a374ce2be6fbaddd41
zhangsan <zhangsan@bikeman.com>
2018-04-19 16:04:11 +0800

    init commit

---
+++ mockdata/postList.js
@@ -0,0 +1,30 @@
+module.exports = {
+  data: [
+    {
+      reply_count: 10,
+      visit_count: 20,
+      last_reply_at: '2017-12-30',
+      author: {
+        loginname: 'James',
+        avatar_url: ''
+      },
+      top:true,
+      good:true,
+      tab:'share'
+    },
+    {
+      reply_count: 10,
+      visit_count: 22,
+      last_reply_at: '2016-12-30',
+      author: {
+        loginname: 'Ivring',
+        avatar_url: ''
+      },
+      top:false,
+      good:true,
+      tab:'job'
+    }
+
+  ]
+
+}
